98c5ad1010cd096094907bb284d50cf33fa4e3b2
Tristan Deschamps <tris790@gmail.com>
2023-02-25 10:49:46 -0500

    Build with zig

---
+++ build.zig
@@ -0,0 +1,61 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const glfwLib = b.addStaticLibrary(.{
+        .name = "glfw",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // Add all files names in the src folder to `files`
+    glfwLib.linkSystemLibrary("pthread");
+    glfwLib.addIncludePath("src");
+    var src = try std.fs.cwd().openDir("src", .{});
+    var dir = try src.openIterableDir(".", .{});
+    var it = dir.iterate();
+    while (try it.next()) |file| {
+        const f: []const u8 = file.name;
+        if (std.mem.endsWith(u8, f, ".c")) {
+            var s: []const u8 = "src/";
+            glfwLib.addCSourceFile(try std.mem.concat(b.allocator, u8, &[2][]const u8{s, f}) , &[_][]const u8{"-D_GLFW_X11"});
+        }
+    }
+    // Add the file names as an option to the exe, making it available
+    // as a string array at comptime in main.zig
+
+    glfwLib.install();
+
+    const exe = b.addExecutable(.{
+        .name = "glfw-test",
+        .root_source_file = .{ .path = "src/exemple.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.addIncludePath("include");
+    exe.linkSystemLibrary("GL");
+    exe.linkLibrary(glfwLib);
+    exe.linkLibC();
+    exe.install();
+
+    const run_cmd = exe.run();
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+
+    const main_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&main_tests.step);
+}
---
+++ src/exemple.zig
@@ -0,0 +1,27 @@
+const std = @import("std");
+// const c = @cImport(@cInclude("../include/glfw3.h"));
+const c = @cImport(@cInclude("GLFW/glfw3.h"));
+
+pub fn main() !void {
+
+    if (c.glfwInit() == 0)
+        return error.FailedInit;
+
+    var window = c.glfwCreateWindow(640, 480, "My Window", null, null);
+    if (false and window == null)
+    {
+        c.glfwTerminate();
+        return error.FailedCreateWindow;
+    }
+
+    c.glfwMakeContextCurrent(window);
+
+    while (c.glfwWindowShouldClose(window) == 0)
+    {
+        c.glClear(c.GL_COLOR_BUFFER_BIT);
+        c.glfwSwapBuffers(window);
+        c.glfwPollEvents();
+    }
+
+    c.glfwTerminate();
+}
