ed7c7dcc06fe8be1930b407891a4e893598d922f
Tristan Deschamps <tris790@gmail.com>
2023-02-25 11:21:10 -0500

    Add installHeadersDirectory

--- build.zig
+++ build.zig
@@ -10,7 +10,6 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     });
 
-    // Add all files names in the src folder to `files`
     glfwLib.linkSystemLibrary("pthread");
     glfwLib.addIncludePath("src");
     var src = try std.fs.cwd().openDir("src", .{});
@@ -19,43 +18,40 @@ pub fn build(b: *std.Build) !void {
     while (try it.next()) |file| {
         const f: []const u8 = file.name;
         if (std.mem.endsWith(u8, f, ".c")) {
-            var s: []const u8 = "src/";
-            glfwLib.addCSourceFile(try std.mem.concat(b.allocator, u8, &[2][]const u8{s, f}) , &[_][]const u8{"-D_GLFW_X11"});
+            glfwLib.addCSourceFile(try std.mem.concat(b.allocator, u8, &[2][]const u8{src/, f}) , &[_][]const u8{"-D_GLFW_X11"});
         }
     }
-    // Add the file names as an option to the exe, making it available
-    // as a string array at comptime in main.zig
 
     glfwLib.install();
+    glfwLib.installHeadersDirectory("include", "glfw");
+    // const exe = b.addExecutable(.{
+    //     .name = "glfw-test",
+    //     .root_source_file = .{ .path = "src/exemple.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
-    const exe = b.addExecutable(.{
-        .name = "glfw-test",
-        .root_source_file = .{ .path = "src/exemple.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
+    // exe.addIncludePath("include");
+    // exe.linkSystemLibrary("GL");
+    // exe.linkLibrary(glfwLib);
+    // exe.linkLibC();
+    // exe.install();
 
-    exe.addIncludePath("include");
-    exe.linkSystemLibrary("GL");
-    exe.linkLibrary(glfwLib);
-    exe.linkLibC();
-    exe.install();
+    // const run_cmd = exe.run();
+    // run_cmd.step.dependOn(b.getInstallStep());
+    // if (b.args) |args| {
+    //     run_cmd.addArgs(args);
+    // }
 
-    const run_cmd = exe.run();
-    run_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
+    // const run_step = b.step("run", "Run the app");
+    // run_step.dependOn(&run_cmd.step);
 
-    const run_step = b.step("run", "Run the app");
-    run_step.dependOn(&run_cmd.step);
+    // const main_tests = b.addTest(.{
+    //     .root_source_file = .{ .path = "src/main.zig" },
+    //     .target = target,
+    //     .optimize = optimize,
+    // });
 
-    const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    // const test_step = b.step("test", "Run library tests");
+    // test_step.dependOn(&main_tests.step);
 }
